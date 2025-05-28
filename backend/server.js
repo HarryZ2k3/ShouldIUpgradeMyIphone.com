e228f5318cf5c354f9a9e875629c33a8ea68c4b0
Harry Huynh <phuongdai.saigon@gmail.com>
2025-05-28 19:25:43 +0700

    continuation

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express         = require('express');
 const mongoose        = require('mongoose');
 const cors            = require('cors');
+const path = require('path');
 const session         = require('express-session');
 const MongoStore      = require('connect-mongo');
 const passport        = require('passport');
